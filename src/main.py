403c19f946c7ee10d12bbd655b3971cd71610a40
DummyJack <s38958091@gmail.com>
2024-11-08 05:22:08 +0800

    固定視窗大小和調整UI

--- src/main.py
+++ src/main.py
@@ -5,15 +5,18 @@ from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from ui import MainView
 
+
 class JapaneseLearnApp(App):
     def build(self):
         # 註冊中文字體
         LabelBase.register(
-            name="ChineseFont", 
-            fn_regular="resources/fonts/NotoSansTC-VariableFont_wght.ttf"
+            name="ChineseFont",
+            fn_regular="resources/fonts/NotoSansTC-VariableFont_wght.ttf",
         )
+        Window.size = (1080, 720)  # 設置固定視窗大小
         Window.clearcolor = (0.94, 0.97, 1, 1)  # 設置窗口背景顏色為淺藍色
         return MainView()
+
 
 if __name__ == "__main__":
     JapaneseLearnApp().run()
